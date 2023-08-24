0fe34d486666c1bf57a68e7528b64f89058f63b7
cvir3 <vir3.chauhan@gmail.com>
2023-08-24 11:50:55 +0530

    Deleted

--- JS/ModuleDemo.js
+++
@@ -1,5 +0,0 @@
-/* This is import module */
-const student = require("./M_Students")
-
-student.showCollName()
-student.showStudentName()
--- JS/ModuleDemo2.js
+++
@@ -1,8 +0,0 @@
-/* This is inbuild module */
-const os=require("os")
-const viren = require("assert")
-console.log(os.platform());
-console.log(os.type());
-console.log(os.hostname());
-
-viren(50>60,"value mismatched")
